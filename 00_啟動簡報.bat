e39f024542caaa74b36bd9d4a39869da103bd79a
ykf7103-rgb <ykf7103@gmail.com>
2026-05-22 14:22:25 +0800

    Add V7 presentation without transfer mechanism slide

--- 00_啟動簡報.bat
+++ 00_啟動簡報.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul
 set "ROOT=%~dp0"
-set "HTML=%ROOT%AI_INFINITY_v6.html"
+set "HTML=%ROOT%AI_INFINITY_v7.html"
 
 if exist "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe" (
   start "" "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe" --new-window "%HTML%"
